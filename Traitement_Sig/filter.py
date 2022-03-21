20e07216229e8da770a070f0bd940ba73cf4db7b
nmassad <nicholas.massad@menya.levio.ca>
2022-03-21 11:12:27 -0400

    added start to filter

---
+++ Traitement_Sig/filter.py
@@ -0,0 +1,12 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import signal
+from scipy.io import wavfile
+from os.path import dirname, join as pjoin
+import winsound
+
+class Filter:
+
+    def __init__(self):
+
--- Traitement_Sig/main.py
+++ Traitement_Sig/main.py
@@ -5,3 +5,6 @@ from scipy import signal
 from scipy.io import wavfile
 from os.path import dirname, join as pjoin
 import winsound
+
+
+if __name__ == '__main__':
